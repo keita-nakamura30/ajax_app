2d345f3da611f4885daa85f871dc26a17a79752c
keita-nakamura30 <keita.618.73@gmail.com>
2021-01-29 08:24:49 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#checked'
+end
+
+def checked
+  post = Post.find(params[:id])
+  if post.checked 
+    post.update(checked: false)
+  else
+    post.update(checked: true)
+  end
+
+  item = Post.find(params[:id])
+  render json: { post: item }
 end
